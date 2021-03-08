b3ec9e866cc53324360cc51c39efe09afbbbdba1
coenen <coenen@aliyun.com>
2021-03-08 17:24:49 +0800

    创建 Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new HomePage());
+void main() => runApp(new HomePage1());
 
 class HomePage extends StatelessWidget {
   @override
@@ -13,6 +13,23 @@ class HomePage extends StatelessWidget {
         ),
         body: new Center(
           child: new Text('Hello World Flutter!'),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter1'),
+        ),
+        body: new Center(
+          child: new Text('1 Hello World Flutter!'),
         ),
       ),
     );
